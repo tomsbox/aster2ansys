66351b529abe2237558cb673e48ad15efd9a6040
tomsbox <44747679+tomsbox@users.noreply.github.com>
2022-10-19 22:22:09 +0200

    test with abaqus input

--- main.py
+++ main.py
@@ -4,41 +4,25 @@ import os
 import meshio
 
 if __name__ == '__main__':
-    # two triangles and one quad
-    points = [
-        [0.0, 0.0],
-        [1.0, 0.0],
-        [0.0, 1.0],
-        [1.0, 1.0],
-        [2.0, 0.0],
-        [2.0, 1.0],
-    ]
-    cells = [
-        ("triangle", [[0, 1, 2], [1, 3, 2]]),
-        ("quad", [[1, 4, 5, 3]]),
-    ]
 
-    mesh = meshio.Mesh(
-        points,
-        cells,
-        # Optionally provide extra data on points, cells, etc.
-        point_data={"T": [0.3, -1.2, 0.5, 0.7, 0.0, -3.0]},
-        # Each item in cell data must match the cells array
-        cell_data={"a": [[0.1, 0.2], [0.4]]},
-    )
-    mesh.write(
-        "foo.med",  # str, os.PathLike, or buffer/open file
-        file_format="med",  # optional if first argument is a path; inferred from extension
-    )
-
-    # Alternative with the same options
-    meshio.write_points_cells("foo.vtk", points, cells)
-
-    filename = os.path.join(os.getcwd(), 'foo.med')
+    filename = os.path.join(os.getcwd(), 'output_from_gmesh_with_pysikal_group_ttt.inp')
     print(f"filename: {filename}")
 
     mesh = meshio.read(
         filename,  # string, os.PathLike, or a buffer/open file
         # file_format="stl",  # optional if filename is a path; inferred from extension
         # see meshio-convert -h for all possible formats
+    )
+
+    mesh.write(
+        "output_from_gmesh_with_pysikal_group_ttt.med",  # str, os.PathLike, or buffer/open file
+        file_format="med",
+        # binary=False# optional if first argument is a path; inferred from extension
+    )
+
+    mesh.write(
+        "klotz_with_physical_out.vtk",  # str, os.PathLike, or buffer/open file
+        file_format="vtk",
+        binary=False
+        # binary=False# optional if first argument is a path; inferred from extension
     )
