05304d37b062b4143c9a120c98d4b04950047dba
tomsbox <44747679+tomsbox@users.noreply.github.com>
2022-10-31 21:44:20 +0100

    Create a real test

--- aster2ansys.py
+++ aster2ansys.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     print("########### Welcome to ansys2aster the mesh converter including groups ###############")
 
     input_file_with_path = "C:/Users/Thomas/Documents/WEBAPP/aster2ansys/tests/example_files/ansys_mesh/Klotz_for_aster_TET10_0p5mm.dat"
-    name_of_the_ouput_file = "Klotz_for_aster_TET10_0p5mm.mail"
+    name_of_the_ouput_file = "Klotz_for_aster_TET10_0p5mm_reference.mail"
 
     mesh = Mesh(input_file_with_path, name_of_the_ouput_file)
     mesh.write_header()
--- tests/test_read_and_write_mesh.py
+++ tests/test_read_and_write_mesh.py
@@ -1,26 +1,32 @@
 from mesh import Mesh
 from pathlib import Path
 import unittest
+import filecmp
 
 current_path = Path(__file__).parent.resolve()
 
 
-class MeshTest(unittest.TestCase):
+class ReadAndWriteMeshCompleteProcessTest(unittest.TestCase):
 
     def setUp(self):
         self.file_name = 'Klotz_for_aster_TET10_0p5mm'
         self.file = current_path / "example_files/ansys_mesh" / (self.file_name + ".dat")
         self.result_file = current_path / "example_files/ansys_mesh" / (self.file_name + ".mail")
+        self.result_file_reference = current_path / "example_files/ansys_mesh" / (self.file_name + "_reference.mail")
 
     def test_mesh_reader(self):
+        # read the ansys mesh
         mesh = Mesh(self.file, self.file_name + ".mail")
+        #write the aster mesh
         mesh.write_header()
         mesh.write_nodes()
         mesh.write_elements()
         mesh.write_set_node()
         mesh.write_set_elements()
         mesh.write_footer()
+
         self.assertTrue(Path.is_file(self.result_file))
+        self.assertTrue(filecmp.cmp(self.result_file, self.result_file_reference, shallow=False))
 
     def tearDown(self):
         Path(self.result_file).unlink()
