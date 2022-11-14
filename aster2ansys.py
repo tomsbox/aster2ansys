b759d253afcdb78e250b6c5af568ec2cf18efd6d
tomsbox <44747679+tomsbox@users.noreply.github.com>
2022-11-14 21:54:29 +0100

    Symm model works

--- aster2ansys.py
+++ aster2ansys.py
@@ -1,10 +1,16 @@
 from mesh import Mesh
+from pathlib import Path
 
 if __name__ == '__main__':
     print("########### Welcome to ansys2aster the mesh converter_examples_by_astk including groups ###############")
 
-    input_file_with_path = "C:/Users/Thomas/Documents/WEBAPP/aster2ansys/tests/example_files/ansys_mesh/Klotz_for_aster_TET10_10mm_statisch_bestimmt.dat"
-    name_of_the_ouput_file = "Klotz_for_aster_TET10_10mm_statisch_bestimmt.mail"
+    current_path = Path(__file__).parent.resolve()
+    print(f"current_path: {current_path}")
+
+    file_name = "Qube_for_aster_TET10_10mm_symm"
+    input_file_with_path = current_path / "tests/example_files/ansys_mesh" / (file_name + ".dat")
+    print(f"input_file_with_path: {input_file_with_path}")
+    name_of_the_ouput_file = file_name + ".mail"
 
     mesh = Mesh(input_file_with_path, name_of_the_ouput_file)
     mesh.write_header()
