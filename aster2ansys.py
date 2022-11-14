d19f9ebcb40a9d052e69502c9c2d1b2d8168bd82
tomsbox <44747679+tomsbox@users.noreply.github.com>
2022-11-14 19:51:41 +0100

    Rename models

--- aster2ansys.py
+++ aster2ansys.py
@@ -3,8 +3,8 @@ from mesh import Mesh
 if __name__ == '__main__':
     print("########### Welcome to ansys2aster the mesh converter_examples_by_astk including groups ###############")
 
-    input_file_with_path = "C:/Users/Thomas/Documents/WEBAPP/aster2ansys/tests/example_files/ansys_mesh/Klotz_for_aster_TET10_0p5mm.dat"
-    name_of_the_ouput_file = "Klotz_for_aster_TET10_0p5mm_reference.mail"
+    input_file_with_path = "C:/Users/Thomas/Documents/WEBAPP/aster2ansys/tests/example_files/ansys_mesh/Klotz_for_aster_TET10_10mm_statisch_bestimmt.dat"
+    name_of_the_ouput_file = "Klotz_for_aster_TET10_10mm_statisch_bestimmt.mail"
 
     mesh = Mesh(input_file_with_path, name_of_the_ouput_file)
     mesh.write_header()
--- tests/test_mesh.py
+++ tests/test_mesh.py
@@ -9,7 +9,7 @@ current_path = Path(__file__).parent.resolve()
 class MeshTest(unittest.TestCase):
     # TODO: Subtests for all example files
     def setUp(self):
-        self.file_name = 'Klotz_for_aster_TET10_0p5mm'
+        self.file_name = 'Qube_for_aster_TET10_0p5mm'
         self.file = current_path / "example_files/ansys_mesh" / (self.file_name + ".dat")
         self.result_file = current_path / "example_files/ansys_mesh" / (self.file_name + ".mail")
         self.result_file_reference = current_path / "example_files/ansys_mesh" / (self.file_name + "_reference.mail")
